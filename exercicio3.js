a33a9836db03d79513920f71748af28c1daccddd
GuilhermeFreire-01 <guilhermefreirecardosodasilva@gmail.com>
2026-05-25 16:19:18 -0300

    switch

--- exercicio3.js
+++ exercicio3.js
@@ -8,3 +8,17 @@ else{
     console.log("Não pode votar");
 }
 //2. Use switch para exibir uma mensagem diferente para cada dia da semana.
+let dia = "segunda-feira";
+
+switch (dia){
+    case 'segunda-feira': console.log("É segunda 😔");break;
+    case 'terca-feira': console.log("AINDA É TERÇA");break;
+    case 'quarta-feira': console.log("HJ É QUARTA FEIRAAAAAAAAAAAAAAA");break;
+    case 'quinta-feira': console.log("Já tá quase, aguenta mais um pouco betinha");break;
+    case 'sexta-feira': console.log("SEXTOOOOOOOOOOOUUUUUUUUU VAMOOOOOOO")
+    case 'sabado': console.log("hoje eu vou pra farra só volto di madrugada...");break;
+    case 'domingo': console.log("Já ki min sino a bebe, ja ki min sino a sofreeee...");break;
+    default: console.log("vc fez o morrimento")
+}
+
+//meu visual code num fumfa fessô 😔
